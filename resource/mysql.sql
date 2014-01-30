7d76812aa8cf633a4eb657e55b90e0774727d3d6
Morrison Laju <morrelinko@gmail.com>
2014-01-30 13:38:28 +0100

    Added setup sql queries

---
+++ resource/mysql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `photo` (
+	`photo_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`storage_name` VARCHAR(50) NOT NULL COLLATE 'utf8_bin',
+	`file_name` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
+	`file_extension` VARCHAR(10) NOT NULL COLLATE 'utf8_bin',
+	`file_path` VARCHAR(100) NOT NULL COLLATE 'utf8_bin',
+	`file_mime` VARCHAR(50) NOT NULL COLLATE 'utf8_bin',
+	`created_at` TIMESTAMP NULL DEFAULT NULL,
+	`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`photo_id`),
+	UNIQUE INDEX `photo_id` (`photo_id`)
+)
+COLLATE='utf8_bin'
+ENGINE=InnoDB;
---
+++ resource/sqlite.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS photo (
+    "photo_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    "storage_name" TEXT NOT NULL,
+    "file_name" TEXT NOT NULL,
+    "file_extension" TEXT NOT NULL,
+    "file_path" TEXT NOT NULL,
+    "file_mime" TEXT NOT NULL,
+    "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME DEFAULT CURRENT_TIMESTAMP
+);
