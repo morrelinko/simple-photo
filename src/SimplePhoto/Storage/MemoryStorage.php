b185b993f9882391aa585330f8d0fda50b474ddd
Morrison Laju <morrelinko@gmail.com>
2014-02-20 12:48:36 +0100

    File headers for MemoryDataStore and MemoryStorage

--- src/SimplePhoto/Storage/MemoryStorage.php
+++ src/SimplePhoto/Storage/MemoryStorage.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SimplePhoto package.
+ *
+ * (c) Laju Morrison <morrelinko@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SimplePhoto\Storage;
 
 use SimplePhoto\Utils\FileUtils;
